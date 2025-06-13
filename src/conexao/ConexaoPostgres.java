beafd0f16684d72559cef68f06b79ecf62550f47
Trappezz <wendley.santos777@gmail.com>
2025-06-12 21:21:27 -0300

    primeiro commit

---
+++ src/conexao/ConexaoPostgres.java
@@ -0,0 +1,29 @@
+package conexao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoPostgres {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/nossas_receitas";
+    private static final String USUARIO = "postgres";
+    private static final String SENHA = "1107";
+
+    public static Connection conectar() {
+        Connection conexao = null;
+
+        try {
+            conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+            System.out.println("Conexão realizada com sucesso!");
+        } catch (SQLException e) {
+            System.err.println("Erro ao conectar com o banco: " + e.getMessage());
+        }
+
+        return conexao;
+    }
+
+    public static void main(String[] args) {
+        conectar();
+    }
+}
+
